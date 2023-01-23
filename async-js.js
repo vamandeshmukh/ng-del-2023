2464d81ff72866582b39e489fe74065087844019
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-01-23 12:42:06 +0530

    js

--- async-js.js
+++ async-js.js
@@ -4,12 +4,20 @@
 // setTimeout(arg1, arg2);
 // setTimeout(() => {}, ms);
 
+// console.log('One');
+// setTimeout(() => { console.log('Two'); }, 2000);
+// console.log('Three');
 
-console.log('One');
-setTimeout(() => { console.log('Two'); }, 2000);
-console.log('Three');
 
+const getMessage = () => {
+    console.log('getMessage invoked.');
+    setTimeout(() => {
+        return { text: 'Have fun!' };
+    }, 2000);
+};
 
+const message = getMessage();
+console.log(message.text);
 
 
 
