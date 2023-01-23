780f609655b22dc2a0f2c3ff73f3f957f4032291
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-01-23 11:16:09 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ng-del-2023
+ JS codes for Deloitte Mumbai trainees 23 Jan 2023
---
+++ script.js
@@ -0,0 +1,16 @@
+
+console.log('Hello world!');
+console.log(10 + 20);
+
+// Java - 
+// int num = 10;
+
+// JavaScript
+// num = 10; // don't use this 
+// var num2 = 20; // don't use this 
+// const num3 = 30; // use this as default choice  
+// let num4 = 40; // use this for "variable" variables
+
+// number, string, boolean 
+
+
